3832e7f67b280388507dc9b54682454202baef29
Soheyl <sashena203@gmail.com>
2025-09-08 14:41:11 +0330

    Improved root page products section

--- src/Data.ts
+++ src/Data.ts
@@ -39,6 +39,46 @@ export const MOCK_CATEGORIES = [
       "This is a short description of the product user sees and interacts with",
     summary_fa: "این یک توضیح کوتاه درباره محصول برای کاربر میباشد",
   },
+  {
+    title_en: "Product name",
+    title_fa: "نام محصول",
+    link: "",
+    alt: "",
+    image: "next.svg",
+    summary_en:
+      "This is a short description of the product user sees and interacts with",
+    summary_fa: "این یک توضیح کوتاه درباره محصول برای کاربر میباشد",
+  },
+  {
+    title_en: "Product name",
+    title_fa: "نام محصول",
+    link: "",
+    alt: "",
+    image: "next.svg",
+    summary_en:
+      "This is a short description of the product user sees and interacts with",
+    summary_fa: "این یک توضیح کوتاه درباره محصول برای کاربر میباشد",
+  },
+  {
+    title_en: "Product name",
+    title_fa: "نام محصول",
+    link: "",
+    alt: "",
+    image: "next.svg",
+    summary_en:
+      "This is a short description of the product user sees and interacts with",
+    summary_fa: "این یک توضیح کوتاه درباره محصول برای کاربر میباشد",
+  },
+  {
+    title_en: "Product name",
+    title_fa: "نام محصول",
+    link: "",
+    alt: "",
+    image: "next.svg",
+    summary_en:
+      "This is a short description of the product user sees and interacts with",
+    summary_fa: "این یک توضیح کوتاه درباره محصول برای کاربر میباشد",
+  },
 ];
 
 export const NAV_LINKS = [
--- src/components/root-page/ProductsSection.tsx
+++ src/components/root-page/ProductsSection.tsx
@@ -1,7 +1,10 @@
+import { RiShareForwardLine } from "react-icons/ri";
+
 import { MOCK_CATEGORIES as data } from "@/Data"; //temporary
 import ProductCard from "@/components/products/ProductCard";
 
 import styles from "./ProductSection.module.css";
+import Link from "next/link";
 
 interface ProductsSectionProps {
   lang: string;
@@ -10,17 +13,28 @@ interface ProductsSectionProps {
 const ProductsSection: React.FC<ProductsSectionProps> = ({ lang }) => {
   return (
     <section className={styles.container}>
-      {data.map((item, index) => (
-        <ProductCard
-          title={lang == "en" ? item.title_en : item.title_fa}
-          link="link"
-          image="/lab-image-2.png"
-          alt="Test text"
-          desc={lang == "en" ? item.summary_en : item.summary_fa}
-          key={index}
-          lang={lang}
-        />
-      ))}
+      <div className={styles.header}>
+        <div className={styles.header_caption}>
+          <h2>Products</h2>
+          <Link href={`${lang}/products`}>
+            All products <RiShareForwardLine />
+          </Link>
+        </div>
+        <hr />
+      </div>
+      <div className={styles.products}>
+        {data.map((item, index) => (
+          <ProductCard
+            title={lang == "en" ? item.title_en : item.title_fa}
+            link="link"
+            image="/lab-image-2.png"
+            alt="Test text"
+            desc={lang == "en" ? item.summary_en : item.summary_fa}
+            key={index}
+            lang={lang}
+          />
+        ))}
+      </div>
     </section>
   );
 };

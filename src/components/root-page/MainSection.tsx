4a5a1c70ec2310a876fbb49120e096b7d44e2ed7
Soheyl Ashena <sashena203@gmail.com>
2025-10-02 16:59:52 +0330

    Styled main banner

--- src/components/root-page/MainSection.tsx
+++ src/components/root-page/MainSection.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-
+import Image from "next/image";
+import styles from "./MainSection.module.css";
 interface MainSectionProps {
   lang: string;
 }
 const MainSection: React.FC<MainSectionProps> = () => {
   return (
-    <section>
-      <div>
-        <div>
+    <section className={styles.container}>
+      <div className={styles.banner}>
+        <div className={styles.texts}>
           <h1>Trusted Medical & Laboratory</h1>
           <p>
             A wide range of consumables and specialized lab equipment with
@@ -18,21 +18,36 @@ const MainSection: React.FC<MainSectionProps> = () => {
           <button>Click</button>
         </div>
 
-        <img src="lab-image-2.png" alt="Banner" />
+        <Image
+          className={styles.image}
+          width={1200}
+          height={800}
+          src="/labor.png"
+          alt="Banner"
+        />
       </div>
 
-      <ul>
+      <ul className={styles.bonus}>
         <li>
-          <p>Authentic products</p>
-          <p>Guarenteed original items</p>
+          <img src="/shield.svg" alt="shield-icon" />
+          <div>
+            <p className={styles.bonusTitle}>Authentic products</p>
+            <p className={styles.bonusDesc}>Guarenteed original items</p>
+          </div>
         </li>
         <li>
-          <p>Fast shipping</p>
-          <p>Quick process and delivery</p>
+          <img src="/streamline.svg" alt="streamline-icon" />
+          <div>
+            <p className={styles.bonusTitle}>Fast shipping</p>
+            <p className={styles.bonusDesc}>Quick process and delivery</p>
+          </div>
         </li>
         <li>
-          <p>Available support</p>
-          <p>We&apos;re here to help, Anytime</p>
+          <img src="/support.svg" alt="support-icon" />
+          <div>
+            <p className={styles.bonusTitle}>Available support</p>
+            <p className={styles.bonusDesc}>We&apos;re here to help, Anytime</p>
+          </div>
         </li>
       </ul>
     </section>

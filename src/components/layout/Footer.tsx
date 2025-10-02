0729af48b6217b4aa59adfdca81044639e0d7c43
Soheyl Ashena <sashena203@gmail.com>
2025-10-02 17:16:50 +0330

    Styled footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,35 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({ lang }) => {
   return (
     <footer className={styles.footer}>
-      <div>{lang}</div>
+      <div className={styles.section}>
+        <img className={styles.logo} src="/logo.svg" alt="logo" />
+      </div>
+
+      <div className={styles.section}>
+        <h3>Title</h3>
+        <hr />
+        <ul className={styles.list}>
+          <li>
+            <a href="#">lalala</a>
+          </li>
+          <li>
+            <a href="#">lalala</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.section}>
+        <h3>Title</h3>
+        <hr />
+        <ul className={styles.list}>
+          <li>
+            <a href="#">lalala</a>
+          </li>
+          <li>
+            <a href="#">lalala</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
